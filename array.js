5c738baa370df0cb2f545c605bebad31ef9a9636
taieba.sanjida <taieba.sanjida@gmail.com>
2025-08-02 18:04:16 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,10 @@
+
+let places = ["sherpur","jamalpur","tangail","mymensingh"]
+places.push("bogura");
+// console.log(places);
+places.push("sylhet","dhaka")
+
+places.pop()
+places.pop()
+
+console.log(places);
