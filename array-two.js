b508959ce69d5741587fb6154a0e0cf281c48394
taieba.sanjida <taieba.sanjida@gmail.com>
2025-08-02 18:33:07 +0600

    array-two.js

---
+++ array-two.js
@@ -0,0 +1,11 @@
+
+let books =["microbiology","genetics","environment-chemistry","bio-technology"]
+let hasBooks=books.includes("genetics")
+
+if(hasBooks){
+    console.log("yes,");
+    
+}
+else{
+    ("no")
+}
--- array.js
+++ array.js
@@ -3,8 +3,10 @@ let places = ["sherpur","jamalpur","tangail","mymensingh"]
 places.push("bogura");
 // console.log(places);
 places.push("sylhet","dhaka")
+places.shift("sherpur")
+places.unshift("gazipur")
 
-places.pop()
-places.pop()
+// places.pop()
+// places.pop()
 
 console.log(places);
